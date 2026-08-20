dcb6f3165f9a9b80534e525d047a0b20db186dde
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2026-08-20 12:06:52 +0530

    Basic JAVA learnings

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables{
+    public static void main(){
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        String name = "Naresh";
+        System.out.println("Name: " + name);
+    }
+}
