34c5e9d349a51893f2d484be94f7526b132a3d02
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2026-08-25 19:28:35 +0530

    Pyramid pattern

--- Pattern.java
+++ Pattern.java
@@ -1,8 +1,31 @@
+import java.util.*;
+
+
 public class Pattern {
     public static void main(String[] args){
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        // Pyramid pattern
+        System.out.println("Enter a number of rows : ");
+        int rows = sc.nextInt();
+
+        for(int i = 1; i <= rows; i++){
+            for (int j = 1; j <= rows - i; j++) {
+            System.out.print(" ");
+        }
+        // Print stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            System.out.print("*");
+        }
+            System.out.println("");
+        }
+        
+
     }
 }
