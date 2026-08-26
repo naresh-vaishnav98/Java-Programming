c4e82460a861e9933d04be9230a7f5d2751cc3d8
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2026-08-26 18:25:17 +0530

    Hollow Rectangle and inverted half pyramid

--- Pattern.java
+++ Pattern.java
@@ -12,20 +12,52 @@ public class Pattern {
         Scanner sc = new Scanner(System.in);
 
         // Pyramid pattern
+        // System.out.println("Enter a number of rows : ");
+        // int rows = sc.nextInt();
+
+        // for(int i = 1; i <= rows; i++){
+        //     for (int j = 1; j <= rows - i; j++) {
+        //     System.out.print(" ");
+        // }
+        // // Print stars
+        // for (int k = 1; k <= 2 * i - 1; k++) {
+        //     System.out.print("*");
+        // }
+        //     System.out.println("");
+        // }
+
+        // Hollow Rectangle pattern
+        // System.out.println("Enter a number of rows : ");
+        // int rows = sc.nextInt();
+
+        // for(int i = 1; i <= rows; i++){
+        //     for(int j = 1; j <= rows; j++){
+        //         if (i == 1 || i == rows) {
+        //             System.out.print("* ");
+        //         }else{
+        //             if (j == 1 || j == rows) {
+        //                 System.out.print("* ");
+        //             }else{
+        //                 System.out.print("  ");
+        //             }
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+
+        // INVERTED & ROTATED HALF-PYRAMID pattern
         System.out.println("Enter a number of rows : ");
         int rows = sc.nextInt();
 
         for(int i = 1; i <= rows; i++){
-            for (int j = 1; j <= rows - i; j++) {
-            System.out.print(" ");
+            for(int j = 1; j <= rows-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        // Print stars
-        for (int k = 1; k <= 2 * i - 1; k++) {
-            System.out.print("*");
-        }
-            System.out.println("");
-        }
-        
-
     }
 }
