c14cd2baa69c5440c1650dad7039a8473e7e90bb
Naresh Vaishnav <nareshvaishnav69@gmail.com>
2026-09-19 19:00:22 +0530

    merge and quick sorting

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -53,9 +53,37 @@ public class DivideAndConquer{
         }
     }
 
+    public static void quickSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        int pidx = pivotIndex(arr,si,ei);
+        quickSort(arr,si,pidx-1);
+        quickSort(arr,pidx+1,ei);
+    }
+
+    public static int pivotIndex(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1;
+        for(int j = si; j < ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
     public static void main(String args[]){
         int arr[] = {3,2,5,4,8,6};
-        mergeSort(arr,0,arr.length-1);
+        // mergeSort(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i]+" ");
         }
